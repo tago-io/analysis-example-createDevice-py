2c31c955c4c214258de2f83edb29728235798de2
Mateus Cardoso <mateus.informatica44@gmail.com>
2023-02-27 16:12:41 -0300

    Fixed script to work.

--- analysis.py
+++ analysis.py
@@ -16,55 +16,75 @@
 # 4 - Generate a new Token with Expires Never.
 # 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
 
-from tago import Analysis
-from tago import Account
-from tago import Utils
-from tago import Device
+from tago import Utils, Analysis, Device, Account
 
-# The function myAnalysis will run when you execute your analysis
-def myAnalysis(context,scope):
+def startAnalysis(context, scope):
+  if not scope:
+    return context.log("The analysis must be triggered by a widget.")
+
+  # reads the value of account_token from the environment variable
   account_token = list(filter(lambda account_token: account_token['key'] == 'account_token', context.environment))
   account_token = account_token[0]['value']
+
   if not account_token:
-   return context.log("Missing account_token Environment Variable.")
+    return context.log("Missing account_token Environment Variable.")
+
   # Instance the Account class
-  my_account = Account(account_token)
+  account = Account(account_token)
+
   # Get the token of the settings device used in the dashboard, then instance the device class.
   # We will use this to send the Validation (feedback) to the dashboard.
-  dashboard_dev_token  = Utils.getTokenByName(my_account,scope[0]["origin"])
-  dashboard_device = Device(dashboard_dev_token)
+  dashboard_dev_token = Utils.getTokenByName(account, scope[0]["device"])
+  dashboard_device = Device(token=dashboard_dev_token)
+
   # Get the variables sent by the widget/dashboard.
-  network_id = list(filter(lambda network_id: network_id['variable'] == 'device_network', scope))
-  network_id = network_id[0]['value']
-  connector_id  = list(filter(lambda connector_id : connector_id ['variable'] == 'device_connector', scope))
-  connector_id = connector_id[0]['value']
-  device_name  = list(filter(lambda device_name : device_name ['variable'] == 'device_name', scope))
-  device_name = device_name[0]['value']
-  device_eui   = list(filter(lambda device_eui  : device_eui  ['variable'] == 'device_eui', scope))
-  device_eui = device_eui[0]['value']
-  if not network_id:
+  device_network = [obj for obj in scope if obj["variable"]  == "device_network"]
+  device_connector = [obj for obj in scope if obj["variable"]  == "device_connector"]
+  device_name = [obj for obj in scope if obj["variable"]  == "device_name"]
+  device_eui = [obj for obj in scope if obj["variable"]  == "device_eui"]
+
+  if not device_network or not device_network[0]["value"]:
     return context.log('Missing "device_network" in the data scope.')
-  if not connector_id:
+  elif not device_connector or not device_connector[0]["value"]:
     return context.log('Missing "device_connector" in the data scope.')
-  if not device_name:
-    return context.log('Missing "device_name" in the data scope.')
-  if not device_eui:
+  elif not device_eui or not device_eui[0]["value"]:
     return context.log('Missing "device_eui" in the data scope.')
-  result = my_account.devices.create({
-    'name': device_name,
-    'serie_number': device_eui,
-    'tags': [
-      { 'key': 'type', 'value': 'sensor' },
-      { 'key': 'device_eui', 'value': device_eui},
+
+  new_device = {
+    "name": device_name[0]["value"],
+    "serie_number": device_eui[0]["value"],
+    "tags": [
+      # You can add custom tags here.
+      { "key": "type", "value": "sensor" },
+      { "key": "device_eui", "value": device_eui[0]["value"] },
     ],
-    'connector': connector_id,
-    'network': network_id,
-    'active': True,
-  })
-  if not result:
-    dashboard_device.insert({ 'variable': 'validation', 'value': 'Error when creating the device', 'metadata': { 'color': 'red' } })
-  my_account.devices.paramSet(result['result']['device_id'], { 'key': 'param_key', 'value': '10', 'sent': False })
-  dashboard_device.insert({ 'variable': 'validation', 'value': 'Device succesfully created!', 'metadata': { 'type': 'success' } })
-  context.log('Device succesfully created. ID:',result['result']['device_id'])
+    "connector": device_connector[0]["value"],
+    "network": device_network[0]["value"],
+    "active": True,
+    "type": "immutable",
+    "chunk_period": "month", # consider change
+    "chunk_retention": 1, # consider change
+  }
+
+  result = account.devices.create(data=new_device)
+  context.log(result)
+  result = result["result"]
+
+  # To add Configuration Parameters to the device:
+  account.devices.paramSet(
+    device_id=result["device_id"], data={ "key": "param_key", "value": "10", "sent": False }
+  )
+
+  # To add any data to the device that was just created:
+  # const device = new Device({ token: result.token })
+  # device.sendData({ variable: 'temperature', value: 17 })
+
+  # Send feedback to the dashboard:
+  dashboard_device.insert(
+    data={"variable": "validation", "value": "Device succesfully created!", "metadata": {"type": "success" } }
+  )
+  context.log(f"Device succesfully created. ID: {result['device_id']}")
+
+
 # The analysis token in only necessary to run the analysis outside TagoIO
-Analysis('1fa28cb1-1ab3-4d91-b578-7f5d3dc631a9').init(myAnalysis)
+Analysis('MY-ANALYSIS-TOKEN-HERE').init(startAnalysis)
