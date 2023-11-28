965cae552ceed2ffa268fdc571c26563e1b8f572
mateuscardosodeveloper <mateus.informatica44@gmail.com>
2023-11-28 18:05:13 -0300

    Update doc string

--- analysis.py
+++ analysis.py
@@ -5,22 +5,11 @@ Creating devices using dashboard
 Using an Input Widget in the dashboard, you will be able to create devices in your account.
 You can get the dashboard template to use here: https://admin.tago.io/template/6143555a314cef001871ec78
 Use a dummy HTTPs device with the dashboard.
-
-Environment Variables
-In order to use this analysis, you must setup the Environment Variable table.
-  account_token: Your account token. Check bellow how to get this.
-
-Steps to generate an account_token:
-1 - Enter the following link: https://admin.tago.io/account/
-2 - Select your Profile.
-3 - Enter Tokens tab.
-4 - Generate a new Token with Expires Never.
-5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
 """
 from datetime import datetime, timedelta
 
 from tagoio_sdk import Analysis, Device, Resources
-from tagoio_sdk.modules.Account.Device_Type import DeviceCreateInfo
+from tagoio_sdk.types import DeviceCreateInfo
 from typing import Optional, TypedDict, Literal
 
 validation_type = Literal["success", "danger", "warning"]
